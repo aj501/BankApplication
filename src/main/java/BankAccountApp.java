d05913e944979907997cb5b586600329889aa6d1
Alex Pham <alex.j.pham@gmail.com>
2021-01-05 00:36:45 -0600

    Created CSV file reader utilities to read new account holders information

--- src/main/java/BankAccountApp.java
+++ src/main/java/BankAccountApp.java
@@ -1,14 +1,35 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+
 public class BankAccountApp {
   public static void main(String[] args) {
 
-    Checking ck1 = new Checking("Alex Pham","123456789", 1500);
-    Savings sv1 = new Savings("Anh Nguyen","4324567868", 2500);
-    System.out.println("**********************");
-    ck1.showInfo();
-    ck1.compound();
-    System.out.println("**********************");
-    sv1.showInfo();
-    sv1.compound();
-    System.out.println("**********************");
+    String file = "NewBankAccounts.csv";
+    //Read in CSV file
+    try {
+      List<String[]> newAccountHolder = utilities.CSV.read(file);
+      System.out.println("NEW ACCOUNT: ");
+      System.out.println("Name\t\t  SSN\t\t AccountType\t InitialDeposit");
+      for (String[] accountHolder : newAccountHolder) {
+
+        String name = accountHolder[0];
+        String sSN = accountHolder[1];
+        String accountType = accountHolder[2];
+        double initialDeposit = Double.parseDouble(accountHolder[3]);
+        if(accountType.equals("Savings")){
+          System.out.println("Open a savings account");
+        }
+        else if (accountType.equals("Checking")){
+          System.out.println("Open a checking account");
+        }
+        else {
+          System.out.println("Account type reading error!");
+        }
+        System.out.println(name + " " + sSN + " \t" + accountType + "\t\t\t$" + initialDeposit);
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
   }
 }
